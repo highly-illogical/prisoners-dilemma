73b1e839e5ef65f4643b031a30bcffaef2012843
Prachi <prachi1916@gmail.com>
2021-07-06 21:47:25 +0530

    Replace losing strategies with winning ones, plot graph

--- play.py
+++ play.py
@@ -1,29 +1,45 @@
-from prisoner import Strategy
+from prisoner import Strategy, playPD
 from random import sample
+import matplotlib.pyplot as plt
 
-def playPD(s1, s2, matrix):
-    outcome = matrix[s1.play()][s2.play()]
-    s1.points += outcome[0]
-    s2.points += outcome[1]
-    s1.history = s1.play()*2+s2.play()
-    s2.history = s2.play()*2+s1.play()
-    s1.games += 1
-    s2.games += 1
-    return outcome
+strats = {
+    "0000": 5,
+    "0101": 25,
+    "1010": 5,
+    "1111": 5
+}
 
-strats = ["0000", "0000", "0000", "0101", "0101", "0101", "0101", "1111"]
-players = [Strategy(s) for s in strats]
+strats_table = {
+    "0000": [strats["0000"]],
+    "0101": [strats["0101"]],
+    "1010": [strats["1010"]],
+    "1111": [strats["1111"]]
+}
 
-m = [[(3,3), (-2,5)], 
-    [(5,-2), (-1,-1)]]
+players = []
 
-for r in range(50):
+for strat, num in strats.items():
+    for i in range(num):
+        players.append(Strategy(strat))
+
+m = [[(3,3), (-1,5)], 
+    [(5,-1), (0,0)]]
+
+for r in range(200):
     for i in range(100):
         p = sample(players, 2)
         for k in range(5):
             playPD(p[0], p[1], m)
     players.sort(key=lambda x: x.points/x.games)
+    strats[players[0].strategy] -= 1
+    strats[players[-1].strategy] += 1
+
+    for s in strats_table:
+        strats_table[s].append(strats[s])
+
     players[0] = Strategy(players[-1].strategy)
 
-for p in players:
-    print(p)
+for s in strats_table:
+    plt.plot(strats_table[s], label=s)
+plt.legend()
+plt.show()
--- prisoner.py
+++ prisoner.py
@@ -12,3 +12,13 @@ class Strategy:
 
     def play(self):
         return int(self.strategy[self.history])
+
+def playPD(s1, s2, matrix):
+    outcome = matrix[s1.play()][s2.play()]
+    s1.points += outcome[0]
+    s2.points += outcome[1]
+    s1.history = s1.play()*2+s2.play()
+    s2.history = s2.play()*2+s1.play()
+    s1.games += 1
+    s2.games += 1
+    return outcome
